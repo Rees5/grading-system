1e14512d02ff0c53850532e518ea11ba3c58e491
Rees Alumasa <reesalumasa@gmail.com>
2019-02-13 11:07:12 +0300

    Add initial changes to grade page

--- js/scripts.js
+++ js/scripts.js
@@ -4,12 +4,17 @@ var add = function(score) {
 var score = parseInt(prompt("Enter score"));
 if (score >= 80) {
   alert("Grade A")
+  alert("Excellent! Keep it up")
 } else if (score >= 60) {
   alert("Grade B")
+  alert("Good, aim towards getting grade A")
 } else if (score >= 40) {
   alert("Grade C")
+  alert("Fair, aim higher")
 } else if (score >= 20) {
   alert("Grade D")
+  alert("poor, work hard towards improving your grade")
 } else {
   alert("Grade E")
+  alert("Total failure, Work harder")
 }
